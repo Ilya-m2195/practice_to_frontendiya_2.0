8e70816f6f6e4270faf27a178f0ab6ea2e0a051c
Ilya_Myasnichenko <129312680+Ilya-m2195@users.noreply.github.com>
2023-11-29 20:12:10 +0300

    connected to firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDjnheeaic6kZwKoRyIi4AsZXIM7hNPLuI',
+  authDomain: 'practice-to-frontendiya-2.firebaseapp.com',
+  projectId: 'practice-to-frontendiya-2',
+  storageBucket: 'practice-to-frontendiya-2.appspot.com',
+  messagingSenderId: '1044243034338',
+  appId: '1:1044243034338:web:b375d6710db4a3c917eebb'
+};
+
+export const app = initializeApp(firebaseConfig);
