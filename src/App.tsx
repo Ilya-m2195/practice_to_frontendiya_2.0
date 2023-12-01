0a4b9044e9d27d9aafaa92d7c331aaa6de0c6e63
Ilya_Myasnichenko <129312680+Ilya-m2195@users.noreply.github.com>
2023-12-01 22:15:37 +0300

    docs: correct README.md, move .env.example in root, add commit-msg

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>Practice to frontendiya</div>;
+  return <div className='App'>Practice to frontendiya in feature branch</div>;
 }
 
 export default App;
